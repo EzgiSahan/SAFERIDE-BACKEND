da100ca54d494f3803c644c4376c86cac487f33e
ezgi <85304304+EzgiSahan@users.noreply.github.com>
2023-07-27 15:53:39 +0300

    Initial

---
+++ database.sql
@@ -0,0 +1,79 @@
+CREATE DATABASE safeRide;
+
+CREATE TABLE users (
+    name varchar(100) NOT NULL,
+    surname varchar(100) NOT NULL,
+    email varchar(100) NOT NULL UNIQUE,
+    phone varchar(100) NOT NULL UNIQUE,
+    password varchar(100) NOT NULL,
+    role varchar(100) NOT NULL,
+    country varchar(100) NOT NULL,
+    city varchar(100) NOT NULL,
+    address varchar(100) NOT NULL,
+    birthdate varchar(100) NOT NULL
+);
+
+CREATE TABLE children (
+    id int PRIMARY KEY NOT NULL UNIQUE,
+    name varchar(100) NOT NULL,
+    surname varchar(100) NOT NULL,
+    email varchar(100) NOT NULL UNIQUE,
+    phone varchar(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE company (
+    id int PRIMARY KEY NOT NULL UNIQUE,
+    name varchar(100) NOT NULL,
+    date_joined DATE NOT NULL,
+    email varchar(100) NOT NULL UNIQUE,
+    phone varchar(100) NOT NULL,
+    password varchar(100) NOT NULL,
+    role varchar(100) NOT NULL,
+    country varchar(100) NOT NULL,
+    city varchar(100) NOT NULL,
+    address varchar NOT NULL
+);
+
+CREATE TABLE companyAdmin (
+    name varchar(100) NOT NULL,
+    surname varchar(100) NOT NULL,
+    email varchar(100) NOT NULL UNIQUE,
+    phone varchar(100) NOT NULL,
+    password varchar(100) NOT NULL,
+    role varchar(100) NOT NULL,
+    country varchar(100) NOT NULL,
+    city varchar(100) NOT NULL,
+    address varchar(100) NOT NULL,
+    birthdate date NOT NULL,
+    company_id int PRIMARY KEY NOT NULL UNIQUE
+);
+
+CREATE TABLE trips (
+    id int NOT NULL UNIQUE,
+    date_started DATE NOT NULL,
+    bus_id int NOT NULL UNIQUE,
+    bus_driver_id int NOT NULL UNIQUE,
+    passenger int[],
+    PRIMARY KEY (id, bus_id, bus_driver_id)
+);
+
+CREATE TABLE bus (
+    id int NOT NULL UNIQUE,
+    model varchar(100) NOT NULL,
+    company_id integer NOT NULL UNIQUE,
+    bus_driver_id varchar(100) NOT NULL UNIQUE,
+    PRIMARY KEY (id, company_id, bus_driver_id)
+);
+
+CREATE TABLE busDriver (
+    id int PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    surname varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    phone varchar(100) NOT NULL
+);
+
+--psql -U postgres
+--\c safeRide
+--\dt
+--heroku pg:psql
---
+++ routes/users-routes.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import pool from '../db.js';
+import bcrypt from 'bcrypt';
+import { authenticateToken } from '../middleware/authorization.js';
+
+const router = express.Router();
+
+router.get('/', authenticateToken, async(req, res) => {
+    try {
+        const users = await pool.query('SELECT * FROM users');
+        res.json({users: users.rows});
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+})
+
+router.post('/', async(req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const newUser = await pool.query(
+            'INSERT INTO users(user_name, user_surname, user_email, user_password, user_role, user_country, user_city, user_address, user_birthdate) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RESTURNING *',
+        [req.body.name, req.body.surname, req.body.email, hashedPassword, req.body.role, req.body.country, req.body.city, req.body.address, req.body.birthdate]);
+        res.json({users: newUser[0]})
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+})
+
+export default router;
