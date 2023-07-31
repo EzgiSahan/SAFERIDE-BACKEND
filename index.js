a6cafecc98389f8ac082cc28ec2ed7da5bae88c4
ezgi <85304304+EzgiSahan@users.noreply.github.com>
2023-07-31 16:44:52 +0300

    all routers

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import userRouter from './routes/users-routes.js';
 import authRouter from './routes/auth-routes.js';
-
+import busRouter from './routes/bus-routes.js';
+import busDriverRouter from './routes/busDriver-routes.js';
+import childrenRouter from './routes/children-routes.js';
+import companyRouter from './routes/company-routes.js';
+import companyAdminRouter from './routes/companyAdmin-routes.js';
+import tripsRouter from './routes/trips-routes.js';
+  
 
 dotenv.config();
 
@@ -22,6 +28,12 @@ app.use(cookieParser());
 
 app.use('/',express.static(join(__dirname,'public')));
 app.use('/api/auth',authRouter);
-app.use('/api/users',userRouter)
+app.use('/api/users',userRouter);
+app.use('/api/bus', busRouter);
+app.use('/api/busDriver', busDriverRouter);
+app.use('/api/children', childrenRouter);
+app.use('/api/company', companyRouter);
+app.use('/api/companyAdmin', companyAdminRouter);
+app.use('/api/trips', tripsRouter);
 
 app.listen(PORT,()=>console.log(`Server is listening http://localhost:${PORT}`));
---
+++ routes/bus-routes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import pool from '../db.js';
+import { authenticateToken } from '../middleware/authorization.js';
+
+const router = express.Router();
+
+router.get('/',authenticateToken, async(req, res) => {
+    try {
+        const users = await pool.query('SELECT * FROM bus');
+        res.json({users: users.rows});
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+})
+
+export default router;
